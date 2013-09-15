bc30492e0a44682b049e325001fd8fa4d8990370
Amy Hua <foramyhua@gmail.com>
2013-09-15 03:23:29 -0700

    Begin New Parallax Portfolio of AmyHua.Me wit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio2::Application.config.secret_token = '2d8a52405824be0ceb3d9ec3d723d6da65af200e6183ef3148b2d037f7d3e6aed337afffb924aa3a5abab84f6bd402d5a101282914f23f566e32e30c7f40f309'
