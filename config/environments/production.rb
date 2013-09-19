764fd6fa518977b5dca22cdfc0175eb0a9818d8a
Amy Hua <foramyhua@gmail.com>
2013-09-18 22:20:07 -0700

    Ensure load of *.js and *.css in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Portfolio2::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = ['*.js', '*.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
